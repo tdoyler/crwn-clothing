55a80c70f6e0d8ce7d55a9fac77e480e2c1ba369
Tony <tdoyler06@hotmail.com>
2023-06-19 05:40:10 +0100

    Cart Context Added to Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,15 +2,18 @@ import { Fragment, useContext } from "react";
 // Fragment acts like a div within react although unlike a div it isn't render to the page. Just used to wrap the rest of the
 // component in
 import { Outlet, Link } from "react-router-dom";
-
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
+	const { isCartOpen } = useContext(CartContext);
 
 	return (
 		<Fragment>
@@ -32,7 +35,9 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
